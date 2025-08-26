809700cd769644e6dc25520c3eb5588305c78c3f
HypoxiE <kosmaer42@gmail.com>
2025-08-26 17:05:07 +0700

    Добавлен переброс портов

--- init.py
+++ init.py
@@ -6,6 +6,21 @@ import sys
 
 with open("docker-compose.yml", "w+") as file:
 	name = os.getlogin()
+	# if "--ui_p" in sys.argv:
+	# 	ui_port = sys.argv[sys.argv.index("--ui_p") + 1]
+	# else:
+	# 	ui_port = input("Укажите порт для ui: ")
+
+	if "--client_p" in sys.argv:
+		inp_ports = sys.argv[sys.argv.index("--client_p") + 1]
+	else:
+		inp_ports = input("Укажите порты для проксирования через пробел в формате 'host_port'\n: ")
+		
+	f_list_ports = []
+	for port in inp_ports.split(" "):
+		f_list_ports.append(f"      - {port}:{port}")
+
+	print("Готово!")
 
 	file.write('''
 services:
@@ -18,7 +33,7 @@ services:
     environment:
       XRAY_VMESS_AEAD_FORCED: "false"
     ports:
-      - "445:445"
+{f_list_ports}
     tty: true
     networks:
       - nginx_network_external
@@ -27,4 +42,4 @@ services:
 networks:
   nginx_network_external:
     external: true
-'''.format(name=name))
+'''.format(name=name, f_list_ports="\n".join(f_list_ports)))
